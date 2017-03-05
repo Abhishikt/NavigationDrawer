8b1d21021a44902718b20761bc24ebfa54ca2ffd
Abhishikt <sabhishikt@gmail.com>
2017-03-05 17:30:43 +0530

    restructure added packages

--- app/src/main/java/com/resultstrack/navigationdrawer1/DashboardFragment.java
+++ app/src/main/java/com/resultstrack/navigationdrawer1/DashboardFragment.java
@@ -7,10 +7,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.resultstrack.navigationdrawer1.model.OfflineData;
 
 
 public class DashboardFragment extends Fragment {
+
 
     public DashboardFragment() {
         // Required empty public constructor
@@ -22,6 +26,11 @@ public class DashboardFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         final View dView =  inflater.inflate(R.layout.fragment_dashboard, container, false);
+        TextView tvRegChild = (TextView) dView.findViewById(R.id.txtTotalChildren);
+        TextView tvComMem = (TextView) dView.findViewById(R.id.txtComMem);
+        TextView tvMom = (TextView) dView.findViewById(R.id.txtMom);
+
+        tvRegChild.setText(OfflineData.getOfflineChildRegistrationData().size() + " / 243" );
 
         Toast.makeText(getContext(), "Hello World", Toast.LENGTH_SHORT).show();
 
--- app/src/main/java/com/resultstrack/navigationdrawer1/model/OfflineData.java
+++ app/src/main/java/com/resultstrack/navigationdrawer1/model/OfflineData.java
@@ -1,9 +1,42 @@
 package com.resultstrack.navigationdrawer1.model;
 
+import android.support.annotation.NonNull;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
 /**
  * Created by abhishikt sk on 3/3/2017.
  */
 
 public class OfflineData {
 
+    public static List<ChildReg> getOfflineChildRegistrationData()
+    {
+        List<ChildReg> lstChildReg = new ArrayList<ChildReg>();
+        try{
+            ChildReg oChildReg = new ChildReg();
+            lstChildReg = oChildReg.getAllChildren();
+            return lstChildReg;
+        }catch (Exception e)
+        {
+            return lstChildReg;
+        }
+    }
+
+    public static List<CommitteeMember> getOfflineCommitteeMemberData()
+    {
+        List<CommitteeMember> lstCommitteeMember = new ArrayList<CommitteeMember>();
+        try{
+            CommitteeMember oCommitteeMember = new CommitteeMember();
+            lstCommitteeMember = oCommitteeMember.getAllMembers();
+            return lstCommitteeMember;
+        }catch (Exception e)
+        {
+            return lstCommitteeMember;
+        }
+    }
 }
